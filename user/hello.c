d50cf8f545e11a2ef065429991d2bb825c21de7a
vos-student <turtles@utexas.edu>
2023-09-30 16:18:36 +0000

    hello.c test file

--- user/hello.c
+++ user/hello.c
@@ -7,4 +7,15 @@ umain(int argc, char **argv)
 {
 	cprintf("hello, world\n");
 	cprintf("i am environment %08x\n", thisenv->env_id);
+
+	cprintf("Lab 0 Test is live.\n");
+
+    envid_t env_id = thisenv->env_id;
+
+	cprintf("I am environment %08x of type %d.\n", env_id, thisenv->env_type);
+    cprintf("And I have ran %d time(s).\n", thisenv->env_runs);
+
+    int ret = sys_env_set_status(env_id, 2);
+
+    cprintf("Setting environment status with sys_env_set_status(), returned %d.\n", ret);
 }
