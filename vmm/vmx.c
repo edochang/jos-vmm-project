1ee4127222e83d582af8c5bc187dd829de6247a0
Edward Chang <edochang@outlook.com>
2023-10-10 16:59:28 +0000

    Merge commit 'e37840066e55e341ef37764b87a969fa9ba91420'

--- vmm/vmx.c
+++ vmm/vmx.c
@@ -60,18 +60,34 @@ bool vmx_sel_resume(int num) {
  */
 bool vmx_check_support() {
 	uint32_t eax, ebx, ecx, edx;
-	cpuid( 0, &eax, &ebx, &ecx, &edx );
+	//cpuid( 0, &eax, &ebx, &ecx, &edx );
 	/* Your code here */
 	// Use the cpuid() function and set eax = 1, to get the
 	// processor feature information 
 	cpuid( 1, &eax, &ebx, &ecx, &edx );
 
+	/*
+	 * From Section 23.6 of the Intel manual: " If CPUID.1:ECX.VMX[bit 5] = 1, 
+	 * then VMX operation is supported." So if the 5th bit of the ecx variable
+	 * after executing cpuid() is a 1, then VMX is supported.
+	 */
+	/* TA Solution 
+	bool vmx_support = (bool)BIT(ecx, 5);
+	if (vmx_support) {
+		cprintf("Processor supports VMX\n");
+	} else {
+		cprintf("Processor does not support VMX\n");
+	}
+	return vmx_support;
+	*/
 	int vmx_bit = BIT( ecx, 5 );
 	if ( vmx_bit == 1 ) {
+		cprintf("Processor supports VMX\n");
 		return true;
 	}
 	//panic("vmx_check_support not implemented\n");
-	cprintf("[VMM] VMX extension not supported.\n");
+	//cprintf("[VMM] VMX extension not supported.\n");
+	cprintf("Processor does not support VMX\n");
 	return false;
 }
 
@@ -90,6 +106,26 @@ bool vmx_check_support() {
  */
 bool vmx_check_ept() {
 	/* Your code here */
+	/* TA Solution
+	uint64_t msr1, msr2;
+	bool secondary_vmx, ept_support;
+
+	// first, check that secondary VMX controls are enabled by checking 
+	// the primary VMX controls in MSR 0x482
+	msr1 = read_msr(IA32_VMX_PROCBASED_CTLS);
+	secondary_vmx = (bool)BIT(msr1, 63);
+	if (secondary_vmx) {
+		// then, check that EPT is actually supported by checking the 
+		// secondary VMX controls in MSR 0x48b
+		msr2 = read_msr(IA32_VMX_PROCBASED_CTLS2);
+		ept_support = (bool)BIT(msr2, 33);
+		if (ept_support) {
+			cprintf("Processor supports EPT\n");
+			return true;
+		}
+	} 
+	return false;
+	*/
 	uint64_t vmx_ctls = read_msr( IA32_VMX_PROCBASED_CTLS );
 	uint64_t vmx_ctls2 = read_msr( IA32_VMX_PROCBASED_CTLS2 );
 	
@@ -98,6 +134,7 @@ bool vmx_check_ept() {
 	int ept_bit = BIT( vmx_ctls2, (32 + 1) );
 
 	if ( activate_secondary_controls == 1 && ept_bit == 1) {
+		cprintf("Processor supports EPT\n");
 		return true;
 	}
     //panic("vmx_check_ept not implemented\n");
@@ -626,6 +663,7 @@ int vmx_vmrun( struct Env *e ) {
 
 	// Hint, Lab 0: The following if statement should be true when the environment has only run once.
 	// Replace the conditional to use your new variable!
+	// if( curenv == NULL) {
 	if( e != NULL && e->env_runs == 1 ) {
 		physaddr_t vmcs_phy_addr = PADDR(e->env_vmxinfo.vmcs);
 
